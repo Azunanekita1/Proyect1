6ef3527eb4df55f81be7212fb605f109087d495a
Maria Zapata <105446825+Azunanekita1@users.noreply.github.com>
2025-07-07 19:17:40 -0500

    Update ejercicios promesas.js

--- Javascript/ejercicios promesas.js
+++ Javascript/ejercicios promesas.js
@@ -1,1 +1,14 @@
-
+const promesa =new Promise((resolve, reject) => {
+const exito =true; //simula si la operacion tiene exito o no.
+    setTimeout(() => {
+        if (exito) {
+            resolve("!Operacion exitosa!");
+        }else { 
+            reject("Hubo un error");
+        }
+    },2000);//Espera 2 segundos
+});
+promesa 
+.then((resultado) => console.log(resultado))//Operacion exitosa
+.catch((error) => console.error(error))//Maneja el error
+.finally(() => console.log("Operacion finalizada!"));
