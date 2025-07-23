5705799d71ef8285f0a52dd7e2b0d84ed9a9d6e5
Azunanekita1 <maria.zapata.negocio@gmail.com>
2025-07-22 20:41:26 -0500

    Manejo de servidores con express

---
+++ Express/servidorsimplificado.js
@@ -0,0 +1,26 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req,res)=>  {
+    res.send ("Hola soy express");
+}
+);
+app.get("/about", (req,res)=>  {
+    res.send ("Esta es la pagina Acerca de ");
+}
+);
+app.get("/contact", (req,res)=>  {
+    res.send ("contactanos en contactenos@gmail.com");
+}
+);
+
+app.get("/services", (req,res)=>  {
+    res.send ("Ofrecemos desarrollo web y consultoria tecnologica");
+}
+);
+
+app.listen(4000, ()=> {
+    console.log("Servidor Escuchando en http://localhost:4000");
+
+});
+
---
+++ src/index.js
@@ -0,0 +1,4 @@
+const chalk =require("chalk");
+
+console.log(chalk.green("Hola con chalk1 estilo"));
+
